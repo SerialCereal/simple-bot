c5c79e3c10207cca54d593300d9a4b6c781e84b1
SerialCereal <30288279+SerialCereal@users.noreply.github.com>
2017-08-09 08:48:32 +0200

    This is all of the code for the bot. Putting the bat file and this in the same folder will allow the bat file to run this code.

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ const client = new Discord.Client();
 });
 // The code above prints a message when the bot is started.
 client.on('message', message => {
-         if(message.content === ('!ping')){
+         if(message.content === (prefix + 'ping')){
         message.channel.send('PONG!');
     }
-    // The code above is constructed of two things: message.content and message.channel.send. Translation: If the content of the message equals the message '!ping!, print a message in the same channel stating 'PONG!'.
-function rolldice() {
+    // The code above is constructed of two things: message.content and message.channel.send. Translation: If the content of the message equals the prefix we've set below and the message '!ping!, print a message in the same channel stating 'PONG!'.
+  let prefix = "!;
+
+  if (!message.content.startsWith(prefix)) return;
+    // The code above creates a prefix !
+    function rolldice() {
   var rand = ['**1.**', '**2.**', '**3.**', '**4.**', '**5.**', '**6.**'];
 
   return rand[Math.floor(Math.random() * rand.length)];
 }
     // This creates a function called rolldice and sets random variables for it.
-if (message.content === ("!rolldice")) {
+if (message.content === (prefix + "rolldice")) {
   message.channel.send('You rolled ' + rolldice());
 }    
     // This code not only executes a response, but also executes the function rolldice by using one of the random variables (refer to comment above).
